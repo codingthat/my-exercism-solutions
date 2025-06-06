59ad3eaf82429d0fd93398eb0d596fea06af5107
root <root@ip-10-1-2-247.eu-west-2.compute.internal>
2025-06-06 09:07:57 +0000

    [Sync Iteration] elixir/nucleotide-count/2

---
+++ solutions/elixir/nucleotide-count/2/lib/nucleotide_count.ex
@@ -0,0 +1,32 @@
+defmodule NucleotideCount do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a DNA strand.
+
+  ## Examples
+
+  iex> NucleotideCount.count(~c"AATAA", ?A)
+  4
+
+  iex> NucleotideCount.count(~c"AATAA", ?T)
+  1
+  """
+  @spec count(charlist(), char()) :: non_neg_integer()
+  def count(strand, nucleotide), do: Enum.count(strand, &(&1 == nucleotide))
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> NucleotideCount.histogram(~c"AATAA")
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec histogram(charlist()) :: map()
+  def histogram(strand), do: histogram(strand, Map.new(@nucleotides, &({&1, 0})))
+
+  defp histogram([], summary), do: summary
+  defp histogram([h | strand], summary), do: histogram(strand, Map.update!(summary, h, &(&1 + 1)))
+
+end
