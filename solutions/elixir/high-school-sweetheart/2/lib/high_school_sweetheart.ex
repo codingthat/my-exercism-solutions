2e7aafc4f6adc08a5c3babcb9c80ffc20e7ea3ff
root <root@ip-10-1-2-247.eu-west-2.compute.internal>
2025-06-06 09:08:09 +0000

    [Sync Iteration] elixir/high-school-sweetheart/2

---
+++ solutions/elixir/high-school-sweetheart/2/lib/high_school_sweetheart.ex
@@ -0,0 +1,26 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name), do: name |> String.trim |> String.first
+  def initial(name), do: String.upcase(first_letter(name)) <> "."
+  def initials(full_name), do: full_name |> String.split() |> Enum.map_join(" ", &initial/1)
+
+  def pair(full_name1, full_name2) do
+    i1 = initials(full_name1)
+    i2 = initials(full_name2)
+    """
+     ******       ******
+   **      **   **      **
+ **         ** **         **
+**            *            **
+**                         **
+**     #{i1}  +  #{i2}     **
+ **                       **
+   **                   **
+     **               **
+       **           **
+         **       **
+           **   **
+             ***
+              *
+"""
+  end
+end
