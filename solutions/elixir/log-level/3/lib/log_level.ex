26bd37005fedbc6e9a6d33efab703c33e38a5c6c
root <root@ip-10-1-2-247.eu-west-2.compute.internal>
2025-06-06 09:08:09 +0000

    [Sync Iteration] elixir/log-level/3

---
+++ solutions/elixir/log-level/3/lib/log_level.ex
@@ -0,0 +1,25 @@
+defmodule LogLevel do
+  def to_label(level, legacy?) do
+    cond do
+      level == 0 and not legacy? -> :trace
+      level == 1                 -> :debug
+      level == 2                 -> :info
+      level == 3                 -> :warning
+      level == 4                 -> :error
+      level == 5 and not legacy? -> :fatal
+      :else -> :unknown
+    end
+  end
+
+  def alert_recipient(level, legacy?) do
+    case to_label(level, legacy?) do
+      :trace   -> false
+      :debug   -> false
+      :info    -> false
+      :warning -> false
+      :error   -> :ops
+      :fatal   -> :ops
+      :unknown -> if(legacy?, do: :dev1, else: :dev2)
+    end
+  end
+end
