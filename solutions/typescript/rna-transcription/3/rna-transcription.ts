94852e494900b03744cd11f706536f196598f441
root <root@ip-10-1-2-247.eu-west-2.compute.internal>
2025-06-06 09:07:56 +0000

    [Sync Iteration] typescript/rna-transcription/3

---
+++ solutions/typescript/rna-transcription/3/rna-transcription.ts
@@ -0,0 +1,15 @@
+export default class Transcriptor {
+    static translations = {
+        G: 'C',
+        C: 'G',
+        T: 'A',
+        A: 'U',
+    }
+    toRna(dna: string) {
+        return dna.split('').map((value: string) => {
+            const nucleotide = Transcriptor.translations[value]
+            if (!nucleotide) { throw new Error('Invalid input DNA.') }
+            return nucleotide
+        }).join('')
+    }
+}
