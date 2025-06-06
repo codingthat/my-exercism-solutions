2b27a196d47fd29f3801171014d47494ebede80f
root <root@ip-10-1-2-247.eu-west-2.compute.internal>
2025-06-06 09:07:55 +0000

    [Sync Iteration] javascript/leap/1

---
+++ solutions/javascript/leap/1/leap.js
@@ -0,0 +1,8 @@
+export default function isLeapYear(year) {
+    const intYear = parseInt(year, 10);
+    if (isNaN(intYear)) { throw new Error("Must provide the year as a number"); }
+    if (!(intYear % 4 === 0)) { return false; }
+    if (intYear % 400 === 0) { return true; }
+    if (intYear % 100 === 0) { return false; }
+    return true;    
+}
