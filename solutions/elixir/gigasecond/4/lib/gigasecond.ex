9d16f42d96b1fd121fab371000181063c93c1274
root <root@ip-10-1-2-247.eu-west-2.compute.internal>
2025-06-06 09:08:04 +0000

    [Sync Iteration] elixir/gigasecond/4

---
+++ solutions/elixir/gigasecond/4/lib/gigasecond.ex
@@ -0,0 +1,14 @@
+defmodule Gigasecond do
+  import NaiveDateTime, only: [from_erl!: 1, add: 2, to_erl: 1]
+  @doc """
+  Calculate a date one billion seconds after an input date.
+  """
+  @spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) ::
+          {{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}
+  def from(erl_datetime) do
+    erl_datetime
+    |> from_erl!
+    |> add(1_000_000_000)
+    |> to_erl
+  end
+end
