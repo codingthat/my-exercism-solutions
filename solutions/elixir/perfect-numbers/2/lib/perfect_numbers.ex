8e2478d524daecec01f4b5408ed93afad6e6b549
root <root@ip-10-1-2-247.eu-west-2.compute.internal>
2025-06-06 09:08:06 +0000

    [Sync Iteration] elixir/perfect-numbers/2

---
+++ solutions/elixir/perfect-numbers/2/lib/perfect_numbers.ex
@@ -0,0 +1,37 @@
+defmodule PerfectNumbers do
+  @doc """
+  Determine the aliquot sum of the given `number`, by summing all the factors
+  of `number`, aside from `number` itself.
+
+  Based on this sum, classify the number as:
+
+  :perfect if the aliquot sum is equal to `number`
+  :abundant if the aliquot sum is greater than `number`
+  :deficient if the aliquot sum is less than `number`
+  """
+
+#next: do only up to sqrt, use seive, etc...
+
+  @spec classify(number :: integer) :: {:ok, atom} | {:error, String.t()}
+  def classify(number) when number < 1, do: {:error, "Classification is only possible for natural numbers."}
+  def classify(number) do
+    case aliquot(number) do
+      ^number -> {:ok, :perfect}
+      x when x > number -> {:ok, :abundant}
+      _ -> {:ok, :deficient}
+    end
+  end
+
+  defp aliquot(1), do: 0
+  defp aliquot(number) do
+    root = ceil(:math.sqrt(number))
+    1 + (2..root-1//1
+      |> Enum.reduce(0, fn potential_factor, sum ->
+        if rem(number, potential_factor) == 0 do
+          sum + potential_factor + div(number, potential_factor)
+        else
+          sum
+        end
+      end)) + if(root*root == number, do: root, else: 0)
+  end
+end
