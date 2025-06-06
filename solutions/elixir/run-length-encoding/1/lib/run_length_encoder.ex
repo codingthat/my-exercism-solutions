4cd0924781f5fa54c36b5966c19f569f66a91fc4
root <root@ip-10-1-2-247.eu-west-2.compute.internal>
2025-06-06 09:07:59 +0000

    [Sync Iteration] elixir/run-length-encoding/1

---
+++ solutions/elixir/run-length-encoding/1/lib/run_length_encoder.ex
@@ -0,0 +1,22 @@
+defmodule RunLengthEncoder do
+  @doc """
+  Generates a string where consecutive elements are represented as a data value and count.
+  "AABBBCCCC" => "2A3B4C"
+  For this example, assume all input are strings, that are all uppercase letters.
+  It should also be able to reconstruct the data into its original form.
+  "2A3B4C" => "AABBBCCCC"
+  """
+  @spec encode(String.t()) :: String.t()
+  def encode(string) do
+    Regex.replace(~r/(([A-Za-z ])\2{1,})/, string, fn _, chars, char ->
+      to_string(String.length(chars)) <> char
+    end)
+  end
+
+  @spec decode(String.t()) :: String.t()
+  def decode(string) do
+    Regex.replace(~r/([0-9]+)([A-Za-z ])/, string, fn _, count, char ->
+      String.duplicate(char, String.to_integer(count))
+    end)
+  end
+end
