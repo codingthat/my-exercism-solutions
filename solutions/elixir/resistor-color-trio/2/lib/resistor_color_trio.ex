226d6af4d0704a0237f1b4d7ce2f37ccb740336c
root <root@ip-10-1-2-247.eu-west-2.compute.internal>
2025-06-06 09:08:17 +0000

    [Sync Iteration] elixir/resistor-color-trio/2

---
+++ solutions/elixir/resistor-color-trio/2/lib/resistor_color_trio.ex
@@ -0,0 +1,26 @@
+defmodule ResistorColorTrio do
+  @colours ~w[black brown red orange yellow green blue violet grey white] |> Enum.map(&String.to_atom/1)
+  @codes @colours |> Enum.with_index()
+  for { prefix, index } <- Enum.with_index(["kilo", "mega", "giga"]) do
+    defguardp unquote(String.to_atom(prefix))(c) when c in [:orange, :yellow, :green]
+    defguardp unquote(String.to_atom(prefix <> "black"))(c) when c in [:red, :orange, :yellow]
+  end
+  # defguardp kilo(c) when c in [:orange, :yellow, :green]
+  # defguardp kiloblack(c) when c in [:red, :orange, :yellow]
+  # defguardp mega(c) when c in [:blue, :violet, :grey]
+  # defguardp megablack(c) when c in [:green, :blue, :violet]
+  # defguardp giga(c) when c == :white
+  # defguardp gigablack(c) when c in [:grey, :white]
+
+  @doc """
+  Calculate the resistance value in ohms from resistor colors
+  """
+  @spec label(colors :: [atom]) :: {number, :ohms | :kiloohms | :megaohms | :gigaohms}
+  def label([c1, :black, c3 | _]) when gigablack(c3), do: {(@codes[c1] * 10) * 10 ** (@codes[c3] - 9), :gigaohms }
+  def label([c1, c2, c3 | _]) when giga(c3), do: {(@codes[c1] * 10 + @codes[c2]) * 10 ** (@codes[c3] - 9), :gigaohms }
+  def label([c1, :black, c3 | _]) when megablack(c3), do: {(@codes[c1] * 10) * 10 ** (@codes[c3] - 6), :megaohms }
+  def label([c1, c2, c3 | _]) when mega(c3), do: {(@codes[c1] * 10 + @codes[c2]) * 10 ** (@codes[c3] - 6), :megaohms }
+  def label([c1, :black, c3 | _]) when kiloblack(c3), do: {(@codes[c1] * 10) * 10 ** (@codes[c3] - 3), :kiloohms }
+  def label([c1, c2, c3 | _]) when kilo(c3), do: {(@codes[c1] * 10 + @codes[c2]) * 10 ** (@codes[c3] - 3), :kiloohms }
+  def label([c1, c2, c3 | _]), do: {(@codes[c1] * 10 + @codes[c2]) * 10 ** @codes[c3], :ohms}
+end
