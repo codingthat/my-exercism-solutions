4f81897822aad4555e114724b4f9e1feb4e53175
root <root@ip-10-1-2-247.eu-west-2.compute.internal>
2025-06-06 09:08:10 +0000

    [Sync Iteration] elixir/german-sysadmin/4

---
+++ solutions/elixir/german-sysadmin/4/lib/username.ex
@@ -0,0 +1,15 @@
+defmodule Username do
+  defp sanitize_char(char) do
+    case char do
+      as_is when as_is == ?_
+              or as_is in ?a..?z -> [as_is]
+      ?ä -> ~c"ae"
+      ?ö -> ~c"oe"
+      ?ü -> ~c"ue"
+      ?ß -> ~c"ss"
+      _else -> []
+    end
+  end
+  
+  def sanitize(username), do: username |> Enum.flat_map(&sanitize_char/1)
+end
