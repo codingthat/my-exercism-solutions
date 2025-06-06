306805c4881aa6e205d946ec2602b81bc0b386eb
root <root@ip-10-1-2-247.eu-west-2.compute.internal>
2025-06-06 09:08:10 +0000

    [Sync Iteration] elixir/german-sysadmin/2

---
+++ solutions/elixir/german-sysadmin/2/lib/username.ex
@@ -0,0 +1,13 @@
+defmodule Username do
+  defp substitute(char) do
+    case char do
+      ?ä -> ~c"ae"
+      ?ö -> ~c"oe"
+      ?ü -> ~c"ue"
+      ?ß -> ~c"ss"
+      other -> [other]
+    end
+  end
+  
+  def sanitize(username), do: username |> Enum.flat_map(&substitute/1) |> Enum.filter(&(&1 == ?_ or &1 >= ?a and &1 <= ?z))
+end
