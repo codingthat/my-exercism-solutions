8e224f8534681b04dcfc0993bb8bce9e372424d5
root <root@ip-10-1-2-247.eu-west-2.compute.internal>
2025-06-06 09:08:13 +0000

    [Sync Iteration] elixir/boutique-inventory/1

---
+++ solutions/elixir/boutique-inventory/1/lib/boutique_inventory.ex
@@ -0,0 +1,25 @@
+defmodule BoutiqueInventory do
+  def sort_by_price(inventory), do: Enum.sort_by(inventory, &(&1[:price]))
+  def with_missing_price(inventory), do: Enum.filter(inventory, &(&1[:price] == nil))
+
+  def update_names(inventory, old_word, new_word),
+    do:
+      Enum.map(inventory, fn item ->
+        Map.update!(item, :name,
+          &String.replace(&1, old_word, new_word))
+      end)
+
+  def increase_quantity(item, count),
+    do:
+      Map.update!(item, :quantity_by_size,
+        &Map.new(&1,
+          fn {k, v} -> { k, v + count } end
+        ))
+
+  def total_quantity(item),
+    do:
+      item[:quantity_by_size]
+      |> Map.values()
+      |> Enum.reduce(0, &Kernel.+/2)
+
+end
